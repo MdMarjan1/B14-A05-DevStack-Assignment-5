0b75388e78191f2a8bd7065a1ca7ae29680c02fe
Marjan <mdmarjan.pg@gmail.com>
2026-09-13 12:32:57 +0600

    your task padding margin fixed

--- DevStack/src/components/Tech/Allcard.tsx
+++ DevStack/src/components/Tech/Allcard.tsx
@@ -11,13 +11,13 @@ interface AllcardProps {
 
 const Allcard = ({ tech }: AllcardProps) => {
   return (
-    <section className="grid md:grid-cols-1  lg:grid-cols-4 gap-2">
+    <section className="grid md:grid-cols-1  lg:grid-cols-4 gap-2 md:mb-7">
       <div className="grid md:grid-cols-1  lg:grid-cols-3 lg:col-span-3 md:gap-4 mx-5">
         {tech.map((tec) => (
           <TechCard key={tec.id} tec={tec} />
         ))}
       </div>
-      <div className="w-64 shrink-0">
+      <div className="w-full shrink-0 mb-4">
         <YourStack />
       </div>
     </section>
--- DevStack/src/components/Tech/YourStack.tsx
+++ DevStack/src/components/Tech/YourStack.tsx
@@ -5,7 +5,7 @@ const YourStack = () => {
   const { stack, removeFromStack, removeAll } = useStack();
 
   return (
-    <div className="w-60 border border-gray-200 rounded-xl p-4">
+    <div className="h-48 border border-gray-200 rounded-xl p-4 m-3">
       <h3 className="font-bold pb-1">Your Stack</h3>
       <p className="text-[#64748B] text-sm pb-3">
         {stack.length === 0
