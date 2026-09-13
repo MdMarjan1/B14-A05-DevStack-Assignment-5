d73c1dd2d739c45e88aae9de5a419e317cad78cd
Marjan <mdmarjan.pg@gmail.com>
2026-09-13 16:46:14 +0600

    header head tag set & responsive

--- DevStack/src/components/Hero.tsx
+++ DevStack/src/components/Hero.tsx
@@ -1,16 +1,16 @@
 import logo from "../assets/banner-stack.png";
 const Hero = () => {
   return (
-    <header className=" container mx-auto flex flex-col md:flex-row md:items-center justify-between">
+    <header className=" container mx-auto flex flex-col md:flex-row md:items-center justify-between mt-8 md:mt-0">
       {/* hero text part */}
       <div className="mx-auto md:mx-0 px-5 md:px-3">
-        <h1 className="text-6xl font-bold pb-7 text-center md:text-start">
+        <h1 className="text-4xl md:text-6xl font-bold pb-7 text-center md:text-start">
           Build Your Ideal <br />
-          <span className="bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent">
+          <span className=" bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent">
             Development Stack
           </span>
         </h1>
-        <p className="text-[#475569] pb-6 text-center md:text-start">
+        <p className="text-[#475569] pb-6 text-center md:text-start text-[1rem] md:text-2xl">
           Explore frontend, backend, database, and tooling options, <br />{" "}
           compare them side by side, and put together the stack that fits your{" "}
           <br /> next project.
--- DevStack/src/components/Tech/Technology.tsx
+++ DevStack/src/components/Tech/Technology.tsx
@@ -10,9 +10,9 @@ const Technology = ({maincourse}:TechnologyProps) => {
     const tech = use(maincourse);
 
   return (
-  <section className="container mx-auto">
+  <section className="container mx-auto my-2 md:my-8">
     {/* title part */}
-    <div className="py-6">
+    <div className="py-6 px-3 md:px-0">
         <h1 className=" text-4xl font-bold pb-3.5">Explore the <span className="bg-linear-to-r from-[#D81B7E] to-[#8B5CF6]  bg-clip-text text-transparent">Technologies</span></h1>
         <p className="text-[#64748B]">Pick one technology per category to build your ideal stack.</p>
     </div>
